369c1a7a37b97664475007ee0c0a07c25635653d
Paulo Henrique Ortolan <portolan@mercadona.com>
2026-03-20 14:54:38 +0000

    demonstration

--- src/main/java/pt/pauloortolan/Main.java
+++ src/main/java/pt/pauloortolan/Main.java
@@ -1,3 +1,36 @@
+import pt.pauloortolan.lists.LazyList;
+import pt.pauloortolan.lists.PartitionedLazyList;
+import pt.pauloortolan.lists.SimpleLazyList;
+
 void main() {
-    System.out.println("Hello and welcome to Functional Programming in Java!");
+    try {
+        List<String> names = Files
+                .lines(Path.of(getClass().getResource("names.txt").getPath()))
+                .toList();
+
+        LazyList<String> lazyList = new SimpleLazyList(names);
+
+        while (!lazyList.isEmpty()) {
+            String name = lazyList.head();
+            System.out.println(name);
+            lazyList = lazyList.tail();
+        }
+
+        PartitionedLazyList partitionedLazyList = new PartitionedLazyList(names, 10);
+
+        int batchNumber = 1;
+        while (!partitionedLazyList.isEmpty()) {
+            IO.println("Batch #%s".formatted(batchNumber++));
+
+            List<String> data = partitionedLazyList.head();
+            for (String name : data) {
+                IO.println("\t%s".formatted(name));
+            }
+
+            partitionedLazyList = (PartitionedLazyList) partitionedLazyList.tail();
+        }
+
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
 }
